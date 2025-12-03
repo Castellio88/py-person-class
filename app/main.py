fff5d80faa8aa40f9f6020433f017975665f3120
Dmytro Bondar <catellio2016@gmail.com>
2025-12-03 12:40:16 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,31 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people):
+    person_objects = []
+
+    # 1. Создаём все объекты Person
+    for data in people:
+        person = Person(data["name"], data["age"])
+        person_objects.append(person)
+
+    # 2. Устанавливаем wife/husband
+    for data in people:
+        person = Person.people[data["name"]]
+
+        spouse_name = data.get("wife") or data.get("husband")
+        if spouse_name is None:
+            continue
+
+        if "wife" in data:
+            person.wife = Person.people[spouse_name]
+        else:
+            person.husband = Person.people[spouse_name]
+
+    return person_objects
